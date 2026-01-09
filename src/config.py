93d3a219f893076175d74e6a7d73ea0f4561d0a6
unknown <robinbegu@gmail.com>
2026-01-09 16:08:31 +0100

    addade en autocreate logmapp ifall den inte existerar

--- src/config.py
+++ src/config.py
@@ -2,4 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG_DIR = os.path.join(BASE_DIR, 'logs')
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
 LOG_FILE = os.path.join(LOG_DIR, 'crypto_tracker.log')
